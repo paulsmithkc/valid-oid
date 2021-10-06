3c356e69b685bffe4ac0e287dc8918f65003d26d
Paul Smith <paulsmithkc@gmail.com>
2021-10-05 21:35:04 -0500

    initial version

---
+++ index.js
@@ -0,0 +1,32 @@
+const { ObjectId } = require('mongodb');
+const { RequestHandler } = require('express');
+
+/**
+ * Validates an ObjectId that is part of the path
+ * and adds the validated ObjectId to the request.
+ *
+ * Calls the next middleware if ObjectId is valid.
+ * Sends a 404 response if the ObjectId is invalid.
+ *
+ * @param {string} paramName the name of the path parameter
+ * @returns {RequestHandler} a middleware
+ */
+function validId(paramName) {
+  return (req, res, next) => {
+    const idString = req.params[paramName];
+    try {
+      if (!idString) {
+        throw new Error('ObjectId was falsy');
+      } else {
+        req[paramName] = new ObjectId(idString);
+        return next();
+      }
+    } catch (err) {
+      return res
+        .status(404)
+        .json({ error: `${paramName} "${idString}" is not a valid ObjectId.` });
+    }
+  };
+}
+
+module.exports = validId;
